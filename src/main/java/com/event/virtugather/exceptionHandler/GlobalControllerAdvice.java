b3d1c911c233199c1f88dd73d76ce9451fb901f0
saumyadip biswas <saumyadip11@gmail.com>
2024-02-16 22:27:12 +0530

    Implement global exception handler class
    Added the "GlobalControllerAdvice" class that handles all exceptions thrown across various controller classes. It contains specific handlers for RuntimeException, IllegalArgumentException, and HttpMessageNotReadableException to return appropriate HTTP status and error message.

--- src/main/java/com/event/virtugather/exceptionHandler/GlobalControllerAdvice.java
+++ src/main/java/com/event/virtugather/exceptionHandler/GlobalControllerAdvice.java
@@ -1,2 +1,27 @@
-package com.event.virtugather.exceptionHandler;public class GlobalControllerAdvice {
+package com.event.virtugather.exceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return new ResponseEntity<>("Required request body is missing, due to "+ ex.getMessage() , HttpStatus.BAD_REQUEST);
+    }
+
 }
