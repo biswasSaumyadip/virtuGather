547b95a601e0fc149c82060a187cd9f5fbe8c191
saumyadip biswas <saumyadip11@gmail.com>
2024-02-16 22:27:52 +0530

    The provided commit message does not fit the code changes described in the diff. A more appropriate commit message could be:
    ```
    Implement registration functionality in LoginController
    
    The LoginController class was expanded to include methods for registering a new user and checking for existing usernames. This was done using a POST method for registration and a GET method for checking usernames. Additionally, error logging capabilities were added for these functionalities.
    ```

--- src/main/java/com/event/virtugather/controller/LoginController.java
+++ src/main/java/com/event/virtugather/controller/LoginController.java
@@ -1,2 +1,38 @@
-package com.event.virtugather.controller;public class LoginController {
+package com.event.virtugather.controller;
+
+import com.event.virtugather.DTO.UserDTO;
+import com.event.virtugather.model.User;
+import com.event.virtugather.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class LoginController {
+    private final UserService userService;
+
+    private static final String NULL_USER_MSG = "User cannot be null";
+    private static final String USER_REG_MSG = "User registered with ID: ";
+
+    @PostMapping("/register")
+    public String register(@RequestBody UserDTO userDTO) {
+        log.info("Attempt to register user");
+        if (userDTO == null) {
+            log.error(NULL_USER_MSG);
+            throw new IllegalArgumentException(NULL_USER_MSG);
+        }
+        User user = userDTO.toUser();
+        user.securePassword(userDTO.getPassword());
+        int userId = userService.createUser(user);
+        log.info(USER_REG_MSG + "{}", userId);
+        return USER_REG_MSG + userId;
+    }
+
+    @GetMapping("/checkUsername/{username}")
+    public boolean checkUsername(@PathVariable String username) {
+        log.info("Checking if username exists: {}", username);
+        return userService.isUsernameExist(username);
+    }
 }
