5d3c42ea6ea1e04856c6cbc28e833fb8be852c04
saumyadip biswas <saumyadip11@gmail.com>
2024-02-22 22:37:54 +0530

    Add DuplicateKeyException handler in GlobalControllerAdvice
    A new exception handler for the DuplicateKeyException has been included in the GlobalControllerAdvice class. By logging the error occurrence sequence, it enhances error reporting. This will respond with a 409 conflict status code and a user-friendly message in the event that a duplicate record creation attempt is made.

--- src/main/java/com/event/virtugather/exceptionHandler/GlobalControllerAdvice.java
+++ src/main/java/com/event/virtugather/exceptionHandler/GlobalControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.event.virtugather.exceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -7,11 +9,19 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<String> handleDuplicationKeyException(DuplicateKeyException ex) {
+        log.error("Duplicate key exception occurred: {}", ex.getMessage());
+        String userFriendlyMessage = "A duplicate record was attempted to be created. Please check your input.";
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(userFriendlyMessage);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
