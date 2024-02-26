b929ee7f5292317d78703ff539802b2a8b0be673
saumyadip biswas <saumyadip11@gmail.com>
2024-02-26 22:37:26 +0530

    Add new SQL queries for events and user details
    Extended DatabaseQueries with additional SQL queries for handling events and user detail operations. The changes include queries for saving an event, getting a user by username, and updating a user's profile picture. This increases the scope of data manipulation in the application and enhances user-related functionalities.

--- src/main/java/com/event/virtugather/constants/SQL/DatabaseQueries.java
+++ src/main/java/com/event/virtugather/constants/SQL/DatabaseQueries.java
@@ -19,4 +19,17 @@ public class DatabaseQueries {
 
     public static final String SAVE_USER_DETAILS_QUERY = "INSERT INTO user_details " +
             "(user_id,first_name, last_name, phone_number, address) VALUE (?,?,?,?,?)";
+
+    public static final String SAVE_EVENT_QUERY = "INSERT INTO events " +
+            "(title, description, start_time, end_time, location, created_by) VALUE (?,?,?,?,?,?)";
+
+    public static final String GET_USER_BY_USERNAME_QUERY = "SELECT ud.user_id, ud.first_name, ud.last_name, " +
+            "ud.phone_number, ud.address, ud.createdAt, ud.updatedAt, ud.profile_picture as imageUrl FROM user_details ud " +
+            "INNER JOIN users ON ud.user_id = users.user_id " +
+            "WHERE username = ?";
+
+    public static final String UPDATE_USER_PROFILE_PICTURE = "UPDATE user_details ud " +
+            "INNER JOIN users ON ud.user_id = users.user_id " +
+            "SET ud.profile_picture = ? " +
+            "WHERE users.username = ?";
 }
