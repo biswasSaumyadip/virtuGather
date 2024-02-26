e90665e9fc2c8fd06410e4130f8251492a3d8f05
saumyadip biswas <saumyadip11@gmail.com>
2024-02-26 22:36:30 +0530

    Implement findByUsername and saveProfileImage methods
    Added the methods findByUsername and saveProfileImage in UserDetailsDaoImpl and made corresponding implementations in their tests. Additionally, created UserDetailsRowMapper to map the ResultSet to UserDetails object. These additions enhance user management capabilities, including retrieving user details by username and saving user profile image.

--- src/main/java/com/event/virtugather/controller/UserController.java
+++ src/main/java/com/event/virtugather/controller/UserController.java
@@ -1,16 +1,28 @@
 package com.event.virtugather.controller;
 
+import com.event.virtugather.exceptions.NotFoundException;
 import com.event.virtugather.model.UserDetails;
+import com.event.virtugather.service.ImageService;
 import com.event.virtugather.service.UserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserDetailsService userDetailsService;
+    private final ImageService imageService;
+
 
     @GetMapping("/user/{id}")
     public UserDetails getUserDetails(@PathVariable long id) {
@@ -26,6 +38,27 @@ public class UserController {
     public int updateUserDetails(@PathVariable long id, @RequestBody UserDetails userDetails) {
         userDetails.setUserId(id);
         return userDetailsService.updateUserDetails(userDetails);
+    }
+
+    @PostMapping("/uploadProfileImage/{username}")
+    public ResponseEntity<?> uploadProfileImage(@RequestBody byte[] imageBytes, @PathVariable String username) throws IOException {
+        imageService.saveProfileImage(imageBytes, username);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/downloadProfileImage/{username}")
+    public ResponseEntity<byte[]> downloadProfileImage(@PathVariable String username) throws IOException {
+        byte[] imageBytes = imageService.downloadProfileImage(username);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(imageBytes.length);
+
+        // Setting Content-Disposition header
+        String fileName = URLEncoder.encode(username + ".jpeg", StandardCharsets.UTF_8);
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"");
+
+        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
     }
 
 }
--- src/test/java/com/event/virtugather/controller/UserControllerTest.java
+++ src/test/java/com/event/virtugather/controller/UserControllerTest.java
@@ -1,6 +1,8 @@
 package com.event.virtugather.controller;
 
+import com.event.virtugather.exceptions.NotFoundException;
 import com.event.virtugather.model.UserDetails;
+import com.event.virtugather.service.ImageService;
 import com.event.virtugather.service.UserDetailsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -9,21 +11,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
 
+
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    ImageService imageService;
 
     @MockBean
     UserDetailsService userDetailsService;
@@ -44,7 +56,7 @@ public class UserControllerTest {
                 .build();
 
         String expectedObject =
-                "{\"detailId\":198,\"userId\":199,\"firstName\":\"blitz\",\"lastName\":\"lilith\",\"phoneNumber\":\"9905892884\",\"address\":null,\"createdAt\":null,\"updatedAt\":null}";
+                "{\"detailId\":198,\"userId\":199,\"firstName\":\"blitz\",\"lastName\":\"lilith\",\"imageUrl\":null,\"phoneNumber\":\"9905892884\",\"address\":null,\"createdAt\":null,\"updatedAt\":null}";
 
         when(userDetailsService.getUserDetails(userId)).thenReturn(userDetails);
 
@@ -96,5 +108,100 @@ public class UserControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    public void getUserDetails_ValidId_ReturnsUserDetails() throws Exception {
+        long validId = 1;
+        UserDetails userDetails = UserDetails.builder()
+                .userId(199L)
+                .detailId(198L)
+                .firstName("blitz")
+                .lastName("lilith")
+                .phoneNumber("9905892884")
+                .build();
+
+        given(userDetailsService.getUserDetails(validId)).willReturn(userDetails);
+
+        mockMvc.perform(get("/user/{id}", validId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is(userDetails.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(userDetails.getLastName())))
+                .andExpect(jsonPath("$.phoneNumber", is(userDetails.getPhoneNumber())));
+    }
+
+    @Test
+    public void givenValidImageAndUserName_whenUploadProfileImage_thenReturnsHttpStatusOk() throws Exception {
+        byte[] imageBytes = "sample image bytes".getBytes();
+
+        doNothing().when(imageService).saveProfileImage(any(byte[].class), any(String.class));
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/uploadProfileImage/testUser")
+                        .content(imageBytes)
+                        .contentType(MediaType.IMAGE_JPEG))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void givenIOException_whenUploadProfileImage_thenReturnsHttpStatusInternalServerError() throws Exception {
+        byte[] imageBytes = "sample image bytes".getBytes();
+
+        doThrow(new IOException()).when(imageService).saveProfileImage(any(byte[].class),any(String.class));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/uploadProfileImage/testUser")
+                        .content(imageBytes)
+                        .contentType(MediaType.IMAGE_JPEG))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isInternalServerError());
+    }
+
+    @Test
+    @DisplayName("Given a valid username, when downloadProfileImage is called, then return HttpStatus.OK")
+    public void givenValidUsername_whenDownloadProfileImage_thenReturnsHttpStatusOk() throws Exception {
+        byte[] imageBytes = "sample image bytes".getBytes();
+        final String USER_PROFILE_DOWNLOAD_URL = "/downloadProfileImage/testUser";
+        when(imageService.downloadProfileImage(any(String.class))).thenReturn(imageBytes);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(USER_PROFILE_DOWNLOAD_URL)
+                .content(imageBytes)
+                .contentType(MediaType.IMAGE_JPEG)
+                )
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().bytes(imageBytes));
+    }
+
+    @Test
+    @DisplayName("Given an IOException, when downloadProfileImage is called, then return HttpStatus.INTERNAL_SERVER_ERROR")
+    public void givenIOException_whenDownloadProfileImage_thenReturnsHttpStatusInternalServerError() throws Exception {
+        final String USER_PROFILE_DOWNLOAD_URL = "/downloadProfileImage/testUser";
+        when(imageService.downloadProfileImage(any(String.class))).thenThrow(IOException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(USER_PROFILE_DOWNLOAD_URL))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isInternalServerError());
+    }
+
+    @Test
+    @DisplayName("Given an NotFoundException, when downloadProfileImage is called, then return HttpStatus.NOT_FOUND")
+    public void givenNotFoundException_whenDownloadProfileImage_thenReturnsNotFoundException() throws Exception {
+        final String USER_PROFILE_DOWNLOAD_URL = "/downloadProfileImage/testUser";
+        when(imageService.downloadProfileImage(any(String.class))).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(USER_PROFILE_DOWNLOAD_URL))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Given a NotFoundException, when downloadProfileImage is called, then return HttpStatus.NOT_FOUND")
+    public void givenNotFoundException_whenDownloadProfileImage_thenReturnsHttpStatusNotFound() throws Exception {
+        final String USER_PROFILE_DOWNLOAD_URL = "/downloadProfileImage";
+        when(imageService.downloadProfileImage(any(String.class))).thenThrow(new NullPointerException());
+
+        mockMvc.perform(MockMvcRequestBuilders.get(USER_PROFILE_DOWNLOAD_URL))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
 
 }
